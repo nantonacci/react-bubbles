c72a42de16c5205586d86e674bbd593bfc8ddd3c
Nicole Antonacci <nicole.antonacci@gmail.com>
2019-10-12 09:11:35 -0700

    Added dependencies, worked on Login and PrivateRoute.

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,8 +1,22 @@
 import React from "react";
+import { axiosWithAuth } from '../utils/axiosWithAuth.js';
 
 const Login = () => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
+
+  login = e => {
+    e.preventDefault();
+
+    axiosWithAuth()
+        .post('/login', this.state.credentials)
+        .then(res => {
+            localStorage.setItem('token', res.data.payload);
+            this.props.history.push('/protected');
+        })
+        .catch(err => console.log(err));
+};
+
   return (
     <>
       <h1>Welcome to the Bubble App!</h1>
